38d05e4a6f989d6ead4e2ebbb0630a15bdc96199
Georgefle <starton25216@gmail.com>
2026-03-12 21:56:13 +0800

    LeetCode56，其中的高级写法十分简洁有效率

--- leetcode/editor/cn/[56]merge-intervals.c
+++ leetcode/editor/cn/[56]merge-intervals.c
@@ -25,39 +25,33 @@
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
 // region
-void swap(int* a, int* b) {
-    int start = a[0];
-    int end = a[1];
-    a[0] = b[0];
-    a[1] = b[1];
-    b[0] = start;
-    b[1] = end;
-}
-
-bool compare(int* a, int* b) {
-    if (a[0] > b[0]) return true;
-    return false;
+void swap(int** a, int** b) {
+    int* tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
 int partition(int** array, int low, int high) {
-    int* pivot = *(array + low);
+    int pivot = array[low][0];
     while (1) {
-        while (compare(pivot, *(array + low))) {
+        while (array[low][0] < pivot) {
             low++;
         }
-        while (compare(*(array + high), pivot)) {
+        while (array[high][0] > pivot) {
             high--;
         }
 
         if (low >= high) return high;
 
-        swap(*(array + low), *(array + high));
+        swap(&array[low], &array[high]);
+        low++;
+        high--;
     }
 }
 
 void quickSort(int** intervals, int low, int high) {
 
-    if (low == high) return;
+    if (low >= high) return;
 
     int bound = partition(intervals, low, high);
     quickSort(intervals, low, bound);
@@ -71,38 +65,55 @@ int** merge(int** intervals, int intervalsSize, int* intervalsColSize, int* retu
     int** ret = malloc(sizeof (int*) * intervalsSize);
     *returnColumnSizes = malloc(sizeof (int) * intervalsSize);
 
-    int* current = intervals[0]; // current保存结算时的区间信息
-    int retIndex = 0; // 记录已经结算的区间个数
-
+    //高级写法,简洁
+    int index = 0;
+    ret[index] = intervals[0];
     for (int i = 1; i < intervalsSize; i++) {
-        int * next = intervals[i];
-        if (current[1] >= next[0]) {
-            // 有重叠，将结束值保存到current中
-            if (current[1] < next[1]) {
-                current[1] = next[1];
-            }
+        if (ret[index][1] >= intervals[i][0]) {
+            if (ret[index][1] < intervals[i][1]) ret[index][1] = intervals[i][1];
         }
-        else { // 没有重叠，进行结算
-            ret[retIndex] = malloc(sizeof (int) * 2);
-            ret[retIndex][0] = current[0];
-            ret[retIndex][1] = current[1];
-            (*returnColumnSizes)[retIndex] = 2;
-            retIndex++; // 已结算区间加1
-            current = next; // 未重叠并且结算后，将下一个需要结算的头区间赋值到current中，使后续操作得以进行
+        else {
+            ret[++index] = intervals[i];
         }
     }
-
-    // 结算最后一个区间
-    ret[retIndex] = (int*)malloc(sizeof(int) * 2);
-    ret[retIndex][0] = current[0];
-    ret[retIndex][1] = current[1];
-    (*returnColumnSizes)[retIndex] = 2;
-    retIndex++;
-
-    // 最后处理一下赋值
-    *returnSize = retIndex;
-
+    *returnSize = index + 1;
+    for (int i = 0; i <= index; i++) {
+        (*returnColumnSizes)[i] = 2;
+    }
     return ret;
+
+    //
+    // int* current = intervals[0]; // current保存结算时的区间信息
+    // int retIndex = 0; // 记录已经结算的区间个数
+    //
+    // for (int i = 1; i < intervalsSize; i++) {
+    //     int * next = intervals[i];
+    //     if (current[1] >= next[0]) {
+    //         // 有重叠，将结束值保存到current中
+    //         if (current[1] < next[1]) {
+    //             current[1] = next[1];
+    //         }
+    //     }
+    //     else { // 没有重叠，进行结算
+    //         ret[retIndex] = malloc(sizeof (int) * 2);
+    //         ret[retIndex][0] = current[0];
+    //         ret[retIndex][1] = current[1];
+    //         (*returnColumnSizes)[retIndex] = 2;
+    //         retIndex++; // 已结算区间加1
+    //         current = next; // 未重叠并且结算后，将下一个需要结算的头区间赋值到current中，使后续操作得以进行
+    //     }
+    // }
+    //
+    // // 结算最后一个区间
+    // ret[retIndex] = (int*)malloc(sizeof(int) * 2);
+    // ret[retIndex][0] = current[0];
+    // ret[retIndex][1] = current[1];
+    // (*returnColumnSizes)[retIndex] = 2;
+    // retIndex++;
+    //
+    // // 最后处理一下赋值
+    // *returnSize = retIndex;
+    // return ret;
 }
 // endregion
 //leetcode submit region end(Prohibit modification and deletion)
@@ -118,11 +129,19 @@ int main() {
     int arraySize;
     int* arrayColSize;
     int** numsSet = parseArraySet(input, &arraySize, &arrayColSize);
-    printf("%d\n", arraySize);
-    printIntArray(arrayColSize, arraySize);
+
+    int returnSize;
+    int* returnColSize;
+    int **returnArrSet = merge(numsSet, arraySize, arrayColSize, &returnSize, &returnColSize);
+    printf("Return the size of the collection array: %d\n", returnSize);
+    printf("An array formed by the length of each array in the collection array: ");
+    printIntArray(returnColSize, returnSize);
     printf("\n");
-    printArrSet(numsSet, arraySize, arrayColSize);
+    printf("result: ");
+    printArrSet(returnArrSet, returnSize, returnColSize);
+    // 释放堆内存
     freeArrSet(numsSet, arraySize, arrayColSize);
+    freeArrSet(returnArrSet, returnSize, returnColSize);
 
     
     return 0;
